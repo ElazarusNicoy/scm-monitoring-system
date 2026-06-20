083a8a7a3f32e5aff5f424f74f51856b67691b41
ElazarusNicoy <aguisandaej@gmail.com>
2026-06-20 22:52:17 +0800

    FEAT: Include pr and woaf critical count retrieve query in the get_critical_transaction_count function

--- db_connection.py
+++ db_connection.py
@@ -108,24 +108,37 @@ def get_critical_transactions_count():
             cursor = conn.cursor()
             
             query = """
-                SELECT
-    (
-        SELECT COUNT(*)
-        FROM mas_transactions_with_threshold
-        WHERE thresholdStatus = 'Critical'
-    )
-    +
-    (
-        SELECT COUNT(*)
-        FROM rcp_transactions_with_threshold
-        WHERE thresholdStatus = 'Critical'
-    )
-    +
-    (
-        SELECT COUNT(*)
-        FROM poacr_transactions_with_threshold
-        WHERE thresholdStatus = 'Critical'
-    ) AS CriticalCount;
+                 SELECT
+            (
+                SELECT COUNT(*)
+                FROM mas_transactions_with_threshold
+                WHERE thresholdStatus = 'Critical'
+            )
+            +
+            (
+                SELECT COUNT(*)
+                FROM rcp_transactions_with_threshold
+                WHERE thresholdStatus = 'Critical'
+            )
+            +
+            (
+                SELECT COUNT(*)
+                FROM poacr_transactions_with_threshold
+                WHERE thresholdStatus = 'Critical'
+            )
+            +
+            (
+                SELECT COUNT(*)
+                FROM pr_transactions_with_threshold
+                WHERE thresholdStatus = 'Critical'
+            )
+            +
+            (
+                SELECT COUNT(*)
+                FROM woaf_transactions_with_threshold
+                WHERE thresholdStatus = 'Critical'
+            )
+            AS CriticalCount;
             """
             cursor.execute(query)
             
